4458e677d2fe68a0f66d0224051f03377ddf5e91
Henrik Wachowitz <ricffb@gmail.com>
2020-06-08 14:07:23 +0200

    update constants

--- player/lib/src/constants.dart
+++ player/lib/src/constants.dart
@@ -1,2 +1,2 @@
-const kPodcastFeed = 'https://ricffb.github.io/feed.xml';
+const kPodcastFeed = 'https://ricffb.github.io/derboardcast/feed.xml';
 const kSpotifyLink = 'https://open.spotify.com/show/3UTiK34aDOOSHFpGQ0RglN';
